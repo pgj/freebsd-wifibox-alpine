cac91cc4d12b4c26d4af985bb013e359b14e16b1
PÁLI Gábor János <pali.gabor@gmail.com>
2022-03-24 20:58:42 +0100

    Remove Chrony and all the services on the `shutdown` runlevel.

--- guest/setup.sh
+++ guest/setup.sh
@@ -9,23 +9,20 @@ rm /etc/motd
 rc-update add networking boot
 rc-update add urandom boot
 rc-update add bootmisc boot
-rc-update add acpid default
-rc-update add modules boot
-rc-update add chronyd default
-rc-update add crond default
-rc-update add devfs sysinit
-rc-update add dmesg sysinit
-rc-update add hostname boot
 rc-update add hwclock boot
-rc-update add hwdrivers sysinit
-rc-update add iptables default
-rc-update add killprocs shutdown
-rc-update add mdev sysinit
-rc-update add savecache shutdown
+rc-update add hostname boot
+rc-update add modules boot
 rc-update add sysctl boot
 rc-update add syslog boot
-rc-update add udhcpd default
 rc-update add wpa_supplicant boot
 rc-update add wpa_passthru boot
+rc-update add acpid default
+rc-update add crond default
+rc-update add iptables default
+rc-update add udhcpd default
+rc-update add devfs sysinit
+rc-update add dmesg sysinit
+rc-update add hwdrivers sysinit
+rc-update add mdev sysinit
 mkdir -p /media/etc
 mkdir -p /media/wpa
